0ceebb7b50ed20e65b2bd6329c702270e129e56a
iremguvendik <iremguvendik@gmail.com>
2022-06-02 09:05:42 +0300

    2snd commit

--- src/gitHub/GitAndGithub.java
+++ src/gitHub/GitAndGithub.java
@@ -5,6 +5,8 @@ public class GitAndGithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Today we learn Github");
+		System.out.println("Today is a good day");
 	}
 
 }
